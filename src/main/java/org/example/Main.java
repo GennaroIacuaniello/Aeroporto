bd2ab1772f228e02a67de8bcf61121f2c0099394
Gennaro Iacuaniello <ge.iacuaniello@studenti.unina.it>
2025-04-11 11:24:22 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,5 @@ public class Main {
 
         System.out.println("Funziona?");
         System.out.println("Ciao");
-        System.out.println("Hello");
-        System.out.println("Hello");
     }
 }
