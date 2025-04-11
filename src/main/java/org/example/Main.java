781284b54b50dd8eadfb851f758e5e344ee7dd1e
Alessandro-uni <alessandro.sorrentino10@studenti.unina.it>
2025-04-11 12:11:46 +0200

    ProvaMaster

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Funziona?");
-        System.out.println("Ciao");
-        System.out.println("java");
-
+        System.out.println("Nuovo Master");
     }
 }
