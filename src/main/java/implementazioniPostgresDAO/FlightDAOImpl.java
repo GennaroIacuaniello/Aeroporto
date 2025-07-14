184a79e9cbe30b3d934a8cff52bcca636930994e
VincenzoRosarioZumbolo <vi.zumbolo@studenti.unina.it>
2025-07-14 19:42:01 +0200

    .

--- src/main/java/implementazioniPostgresDAO/FlightDAOImpl.java
+++ src/main/java/implementazioniPostgresDAO/FlightDAOImpl.java
@@ -9,31 +9,20 @@ import java.util.Date;
 
 public class FlightDAOImpl implements FlightDAO {
 
-    private Connection connection;
-
-    public FlightDAOImpl() throws SQLException {
-
-        try {
-
-            connection = ConnessioneDatabase.getInstance().getConnection();
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-        }
-    }
+    public FlightDAOImpl() throws SQLException {}
 
     public void getImminentArrivingFlights (ArrayList<String> parId, ArrayList<String> parCompanyName, ArrayList<Date> parDate,
                                             ArrayList<Time> parDepartureTime, ArrayList<Time> parArrivalTime, ArrayList<String> parStatus,
                                             ArrayList<Integer> parMaxSeats, ArrayList<Integer> parFreeSeats, ArrayList<Integer> parGate) {
 
-        String query = "SELECT id_flight, company_name, flight_date, departure_time, arrival_time, flight_status," +
-                       "max_seats, free_seats, destination_or_origin, flight_delay, id_gate" +
-                       "FROM FLIGHT" +
-                       "WHERE flight_type = false AND flight_status <> 'landed' AND flight_status <> 'cancelled'" +
-                       "ORDER BY flight_date" +
-                       "LIMIT 10;";
+        try (Connection connection = ConnessioneDatabase.getInstance().getConnection()) {
 
-        try {
+            String query = "SELECT id_flight, company_name, flight_date, departure_time, arrival_time, flight_status," +
+                           "max_seats, free_seats, destination_or_origin, flight_delay, id_gate" +
+                           "FROM FLIGHT" +
+                           "WHERE flight_type = false AND flight_status <> 'landed' AND flight_status <> 'cancelled'" +
+                           "ORDER BY flight_date" +
+                           "LIMIT 10;";
 
             PreparedStatement preparedQuery = connection.prepareStatement(query);
 
