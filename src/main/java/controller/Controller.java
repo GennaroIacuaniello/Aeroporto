7b11cc2f8b5c6d62f40d76e9d51c4a167e36eb5a
Gennaro Iacuaniello <ge.iacuaniello@studenti.unina.it>
2025-07-21 22:10:25 +0200

    Some changes

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1172,10 +1172,11 @@ public class Controller {
     public void getAllLuggagesForABooking(Integer index) {
 
 
-        /*flightController.setBookingResultSelectedFlight(bookingController.getSearchBookingResult().get(index).getBookedFlight().getId());
+        flightController.setBookingResultSelectedFlight(bookingController.getSearchBookingResult().get(index).getBookedFlight().getId());
 
         bookingController.setBookingResultSelectedBooking(index);
 
+        ArrayList<String> ticketNumbers = new ArrayList<>();
         ArrayList<Integer> luggageIds = new ArrayList<>();
         ArrayList<String> luggageIdsAfterCheckin = new ArrayList<>();
         ArrayList<String> luggageTypes = new ArrayList<>();
@@ -1185,83 +1186,23 @@ public class Controller {
             LuggageDAO luggageDAO = new LuggageDAOImpl();
 
 
-            luggageDAO.getAllLuggagesOfBooking(bookingController.getId(), luggageIds, luggageTypes, luggageStatus, luggageIdsAfterCheckin);
+            luggageDAO.getAllLuggagesOfBooking(bookingController.getId(), ticketNumbers, luggageIds, luggageTypes, luggageStatus, luggageIdsAfterCheckin);
 
 
         } catch (SQLException e) {
+            e.printStackTrace();
             new FloatingMessage("Errore nella connessione al Database (Prenotazioni)!", errorButton, FloatingMessage.ERROR_MESSAGE);
         }
 
-        customerController.setSearchBookingResultCustomers(new ArrayList<>());
-        customerController.setSearchBookingResultCustomersIds(new ArrayList<>());
-
-        bookingController.setSearchBookingResult(new ArrayList<>());
-        bookingController.setSearchBookingResultIds(new ArrayList<>());
-
-
-        ticketController.setSearchBookingResult(new ArrayList<>());
-        passengerController.setSearchBookingResult(new ArrayList<>());
-
-        luggageController.setSearchBookingResult(new ArrayList<>());
-        luggageController.setSearchBookingResultIds(new ArrayList<>());
-
-
-        ArrayList<Integer> actualBuyersIds = new ArrayList<>();
-        ArrayList<Integer> actualBookingIds = new ArrayList<>();
-        ArrayList<String> actualTicketNumbers = new ArrayList<>();
-        ArrayList<String> actualSSNs = new ArrayList<>();
-
         try{
 
-            if(flightGates.getFirst() != null){
-                flightController.getFlight().setGate(new Gate(flightGates.getFirst().byteValue()));
-            }
+            luggageController.setSearchBookingResult(new ArrayList<>());
+            luggageController.setSearchBookingResultIds(new ArrayList<>());
 
-            for(int i = 0; i < luggageIds.size(); i++){
-
-
-                if(!actualBuyersIds.contains(buyerIds.get(i))){
-
-                    actualBuyersIds.add(buyerIds.get(i));
-
-                    customerController.getSearchBookingResultCustomers().add(new Customer(usernames.get(i), mails.get(i), hashedPasswords.get(i)));
-                    customerController.getSearchBookingResultCustomersIds().add(buyerIds.get(i));
-                }
-
-
-                if(!actualBookingIds.contains(bookingIds.get(i))){
-
-                    actualBookingIds.add(bookingIds.get(i));
-
-                    bookingController.getSearchBookingResult().add(new Booking(BookingStatus.valueOf(bookingStatus.get(i)), bookingDates.get(i),
-                            customerController.getSearchBookingResultCustomerById(buyerIds.get(i)), flightController.getFlight(),
-                            ticketNumbers.get(i), seats.get(i), checkedIns.get(i),
-                            firstNames.get(i), lastNames.get(i), passengerSSNs.get(i), birthDates.get(i)));
-
-                    bookingController.getSearchBookingResultIds().add(bookingIds.get(i));
-
-                    actualTicketNumbers.add(ticketNumbers.get(i));
-                    ticketController.getSearchBookingResult().add(bookingController.getSearchBookingResult().getLast().getTickets().getLast());
-                    Passenger tmp = ticketController.getSearchBookingResult().getLast().getPassenger();
-                    if(!actualSSNs.contains(tmp.getSSN())){
-                        actualSSNs.add(tmp.getSSN());
-                        passengerController.getSearchBookingResult().add(tmp);
-                    }
-                }else{
-                    if(!actualTicketNumbers.contains(ticketNumbers.get(i))) {
-                        actualTicketNumbers.add(ticketNumbers.get(i));
-                        ticketController.getSearchBookingResult().add(new Ticket(ticketNumbers.get(i), seats.get(i), checkedIns.get(i), flightController.getFlight(),
-                                bookingController.getSearchBookingResultBooksById(bookingIds.get(i)),
-                                firstNames.get(i), lastNames.get(i), passengerSSNs.get(i), birthDates.get(i)));
-                        Passenger tmp = ticketController.getSearchBookingResult().getLast().getPassenger();
-                        if (!actualSSNs.contains(tmp.getSSN())) {
-                            actualSSNs.add(tmp.getSSN());
-                            passengerController.getSearchBookingResult().add(tmp);
-                        }
-                    }
-                }
+            for(int i = 0; i < ticketNumbers.size(); i++){
 
                 if(luggageIds.get(i) != null){
+
                     if(luggageTypes.get(i) != null){
                         luggageController.getSearchBookingResult().add(new Luggage(luggageIdsAfterCheckin.get(i), LuggageType.valueOf(luggageTypes.get(i)), LuggageStatus.valueOf(luggageStatus.get(i)),
                                 ticketController.getSearchBookingResultTicketByTicketNumber(ticketNumbers.get(i))));
@@ -1271,11 +1212,9 @@ public class Controller {
                     }
 
                     ticketController.getSearchBookingResultTicketByTicketNumber(ticketNumbers.get(i)).getLuggages().add(luggageController.getSearchBookingResult().getLast());
+
                 }
-
             }
-
-            flightController.getFlight().getBookings().addAll(bookingController.getSearchBookingResult());
 
             luggageController.getSearchBookingResultIds().addAll(luggageIds);
 
@@ -1283,7 +1222,7 @@ public class Controller {
             e.printStackTrace();
             new FloatingMessage("Errore nella connessione al Database (Bagagli smmarriti)!", errorButton, FloatingMessage.ERROR_MESSAGE);
 
-        }*/
+        }
 
     }
 }
